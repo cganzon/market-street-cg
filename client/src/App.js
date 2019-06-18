fe9006ba088b4d81f6ae89f72bdbec9816b4b1cf
cganzon <crganzon@gmail.com>
2019-06-18 16:51:39 -0700

    implemented react router for navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div>
       <Navbar />
-      {/* <h1>Market Street</h1> */}
     </div>
   );
 }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from "react-router-dom";
 import "./Navbar.css";
+import Home from "../../pages/Home/Home";
+import Login from "../../pages/Login/Login";
 
 class Navbar extends Component {
     state = {
@@ -8,16 +15,20 @@ class Navbar extends Component {
 
     render() {
         return (
-            <nav>
-                <a href="" id="navbar-title">Market Street</a>
-                <div id="user-interface">
-                    <a href="" id="login">Log in</a>
-                    <form id="search-form">
-                        <input type="text" id="search-input" />
-                        <button type="submit" id="search">Search</button>
-                    </form>
-                </div>
-            </nav>
+            <Router>
+                <nav>
+                    <Link to="/" id="navbar-title">Market Street</Link>
+                    <div id="user-interface">
+                        <Link to="/login" id="login">Log in</Link>
+                        <form id="search-form">
+                            <input type="text" id="search-input" />
+                            <button type="submit" id="search">Search</button>
+                        </form>
+                    </div>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/login" component={Login} />
+                </nav>
+            </Router>
         );
     };
 };
